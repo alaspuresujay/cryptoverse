c830e21c9eb8a883edaa6a608879ff46686d93a3
alaspuresujay <alapsuresujay@gmail.com>
2021-12-23 20:16:36 +0530

    base UI

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "antd/dist/antd.css";
+ReactDOM.render(
+  <Router>
+    <App />
+  </Router>,
 
-ReactDOM.render(<App />, document.getElementById("root"));
+  document.getElementById("root")
+);
---
+++ src/services/cryptoApi.js
@@ -0,0 +1,8 @@
+// var options = {
+//   method: "GET",
+//   url: "https://coinranking1.p.rapidapi.com/exchanges",
+//   headers: {
+//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+//     "x-rapidapi-key": "4e6766ff03msh8c00a941a00bd62p1f5d5ejsn6094a5b56fcf",
+//   },
+// };
